cd859289cf85065784a203a0623813ee158e28f4
SaahilNZ <saahilh@outlook.co.nz>
2019-04-14 20:02:34 +1200

    Move game initialisation code into a startGame method

--- src/SpaceExplorer/Game.java
+++ src/SpaceExplorer/Game.java
@@ -27,25 +27,29 @@ public class Game {
 		return allItems;
 	}
 	
-	public static void main(String[] args) {
+	private void startGame() {
 		NameGenerator planetNameGenerator = new NameGenerator(StaticData.PLANET_PREFIXES,
 				StaticData.PLANET_NAMES, StaticData.PLANET_SUFFIXES);
 		
 		NameGenerator outpostNameGenerator = new NameGenerator(null, StaticData.OUTPOST_NAMES,
 				StaticData.OUTPOST_SUFFIXES, false, true, 0, 100);
 		
-		Game game = Game.getCurrentGame();
 		for (int i = 0; i < 10; i++) {
 			Outpost outpost = new Outpost(outpostNameGenerator.generateName());
-			game.planets.add(new Planet(planetNameGenerator.generateName(), outpost));
+			planets.add(new Planet(planetNameGenerator.generateName(), outpost));
 		}
 		
 		System.out.println("Space Explorer");
 		System.out.println();
 		System.out.println("Planets:");
 		
-		for (Planet planet : game.planets) {
+		for (Planet planet : planets) {
 			System.out.println(planet.toString());
 		}
+	}
+	
+	public static void main(String[] args) {
+		Game game = Game.getCurrentGame();
+		game.startGame();
 	}
 }
