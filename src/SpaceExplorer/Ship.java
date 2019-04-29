bd5e276d5b902c5b2728c0df8a925f10abb4532d
SaahilNZ <saahilh@outlook.co.nz>
2019-04-29 15:12:57 +1200

    Add shield fields to the Ship class

--- src/SpaceExplorer/Ship.java
+++ src/SpaceExplorer/Ship.java
@@ -4,11 +4,15 @@ public class Ship {
 	private String name;
 	private int health;
 	private int maxHealth;
+	private int shield;
+	private int maxShield;
 	
-	public Ship(String name, int maxHealth) {
+	public Ship(String name, int maxHealth, int maxShield) {
 		this.name = name;
 		this.maxHealth = maxHealth;
 		this.health = maxHealth;
+		this.maxShield = maxShield;
+		this.shield = maxShield;
 	}
 	
 	public String getName() {
@@ -24,10 +28,21 @@ public class Ship {
 		health = Math.min(maxHealth, health + amount);
 		return health;
 	}
+
+	public int damageShield(int amount) {
+		shield = Math.max(0, shield - amount);
+		return shield;
+	}
+
+	public int repairShield(int amount) {
+		shield = Math.min(maxShield, shield + amount);
+		return shield;
+	}
 	
 	public void viewStatus() {
 		System.out.println("Starship " + name);
 		System.out.println("Health: " + health + "/" + maxHealth);
+		System.out.println("Shield: " + shield + "/" + maxShield);
 		System.out.println();
 	}
 	
