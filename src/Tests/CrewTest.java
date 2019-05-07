ed723fbeb7da67604e55f41b0d47185b00570948
SaahilNZ <saahilh@outlook.co.nz>
2019-05-07 16:25:19 +1200

    Fix crew tests

--- src/Tests/CrewTest.java
+++ src/Tests/CrewTest.java
@@ -30,27 +30,20 @@ class CrewTest {
 		testDummy2 = new Scout("Michael Scott");
 		testDummy3 = new Scout("Izaro");
 		dinglebud = new Ship("Dinglebud", 200, 200);
-		ArrayList<CrewMember> testCrew = new ArrayList<CrewMember>();
+		testCrew = new ArrayList<CrewMember>();
 		testCrew.add(testDummy1);
 		testCrew.add(testDummy2);
 		testCrew.add(testDummy3);
-		//Crew canterbury = new Crew(testCrew, "Canterbury", dinglebud, 0, items);
-		//Placing ^this here leaves the crew 'canterbury' unused, even in the 
-		//assertEquals test cases, so I've resorted to creating it in the test cases themselves
-		
-		
+		canterbury = new Crew(testCrew, "Canterbury", dinglebud, 0, items);
 	}
 	
 	@Test
 	public void createCrew() {
-		Crew canterbury = new Crew(testCrew, "Canterbury", dinglebud, 0, items);
 		assertEquals(testCrew, canterbury.getCrewMembers());
 	}
 	
 	@Test
 	public void memberRemoval() {
-		//The NullPointerException appears in this test case. Not exactly sure why.
-		Crew canterbury = new Crew(testCrew, "Canterbury", dinglebud, 0, items);
 		canterbury.removeMember(testDummy1);
 		testCrew.remove(testDummy1);
 		assertEquals(testCrew, canterbury.getCrewMembers());
@@ -58,8 +51,6 @@ class CrewTest {
 	
 	@Test
 	public void moneyTester() {
-		//Tests adding money
-		Crew canterbury = new Crew(testCrew, "Canterbury", dinglebud, 0, items);
 		canterbury.receiveMoney(200);
 		assertEquals(200, canterbury.currentMoney());
 		
