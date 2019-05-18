501a0804984d747316fe07dd9d6315b1a226c1f8
Isaac <isaac132w@gmail.com>
2019-05-18 16:57:18 +1200

    Create javadoc for the game class

--- src/SpaceExplorer/Game.java
+++ src/SpaceExplorer/Game.java
@@ -6,6 +6,14 @@ import SpaceExplorer.CrewMembers.CrewMember;
 import SpaceExplorer.Utilities.NameGenerator;
 import SpaceExplorer.Utilities.StaticData;
 
+/**
+ * Game class used to set up the game and keep track of the other classes used to play. Also calculates
+ * player's final score and stores all the items that are possible to obtain in the game.
+ * 
+ * @author Saahil Hari and Isaac Walton
+ * @version 1.0, May 2019
+ *
+ */
 public class Game {
 	public static final int STARTING_MONEY = 200;
 
@@ -21,12 +29,19 @@ public class Game {
 	private int totalShipParts;
 	private boolean gameWon = false;
 	private int crewCount;
-
+	
+	/**
+	 * Constructs a new ArrayList of items and planets
+	 */
 	private Game() {
 		planets = new ArrayList<Planet>();
 		allItems = new ArrayList<Item>();
 	}
 	
+	/**
+	 * Used to get the instance of the current game
+	 * @return					The current game instance
+	 */
 	public static Game getCurrentGame() {
 		if (instance == null) {
 			instance = new Game();
@@ -34,38 +49,75 @@ public class Game {
 		return instance;
 	}
 	
+	/**
+	 * Getter for the crew of this game instance
+	 * @return					The crew for this game
+	 */
 	public Crew getCrew() {
 		return crew;
 	}
 	
+	/**
+	 * Getter for the planets available to visit in the game
+	 * @return					The planets available to visit
+	 */
 	public ArrayList<Planet> getPlanets() {
 		return planets;
 	}
 	
+	/**
+	 * Getter for the current planet
+	 * @return					The current planet
+	 */
 	public Planet getCurrentPlanet() {
 		return this.currentPlanet;
 	}
 	
+	/**
+	 * Changes which planet the player is currently at
+	 * @param planet			The planet that the game will move on to
+	 */
 	public void setCurrentPlanet(Planet planet) {
 		this.currentPlanet = planet;
 	}
 	
+	/**
+	 * Getter for the current list of items available in the game
+	 * @return
+	 */
 	public ArrayList<Item> getItems() {
 		return allItems;
 	}
-
+	
+	/**
+	 * Getter for the current day in the game
+	 * @return					The current day in game
+	 */
 	public int getCurrentDay() {
 		return currentDay;
 	}
 	
+	/**
+	 * Getter for the total number of ship parts for the current game
+	 * @return					The number of ship parts
+	 */
 	public int getTotalShipParts() {
 		return totalShipParts;
 	}
 	
+	/**
+	 * Changes the current planet to the first planet in the list of available planets.
+	 * Used when the game first begins
+	 */
 	public void startGame() {
 		currentPlanet = planets.get(0);
 	}
 	
+	/**
+	 * Sets up the game to be played. 
+	 * @param days			Number of days the player would like to play
+	 * @param crew			The crew members the player would like to play with
+	 */
 	public void setupGame(int days, Crew crew) {
 		this.gameWon = false;
 		this.desiredDays = days;
@@ -77,7 +129,10 @@ public class Game {
 		generatePlanets();
 		startGame();
 	}
-
+	
+	/**
+	 * Generates a list of random planets for the player to visit
+	 */
 	private void generatePlanets() {
 		NameGenerator planetNameGenerator = new NameGenerator(StaticData.PLANET_PREFIXES,
 				StaticData.PLANET_NAMES, StaticData.PLANET_SUFFIXES);
@@ -92,12 +147,19 @@ public class Game {
 		}
 	}
 	
+	/**
+	 * Creates a list of food and medical items available to the player
+	 */
 	private void createItems() {
 		allItems = new ArrayList<Item>();
 		createMedicalItems();
 		createFoodItems();
 	}
 	
+	/**
+	 * A list of all medical items available to the player in game. To introduce more Medical items into
+	 * the game, you put them into this method
+	 */
 	private void createMedicalItems() {
 		MedicalItem fullRestore = ItemFactory.createMedicalItem("Full Restore",
 				50, false, true, 100, true);
@@ -119,6 +181,10 @@ public class Game {
 		allItems.add(rustedSyringe);
 	}
 	
+	/**
+	 * A list of all food items available to the player in game. To introduce more food items into
+	 * the game, you put them into this method
+	 */
 	private void createFoodItems() {
 		FoodItem steakBurger = ItemFactory.createFoodItem("Steak Burger",
 				"Roasted Steak Burger", 25, false, true, 60, 0);
@@ -172,10 +238,18 @@ public class Game {
 		return gameStatus;
 	}
 	
+	/**
+	 * Calls the pruneCrewMembers method of the crew to remove dead crew members
+	 * @return			False if no crew members remain in the crew
+	 */
 	public boolean pruneCrewMembers() {
 		return crew.pruneCrewMembers() > 0;
 	}
 	
+	/**
+	 * Calculates the player's score once the game has completed
+	 * @return			The player's final score
+	 */
 	public int calculateScore() {
 		int score = 10000;
 		for (Item item : crew.getItems()) {
@@ -200,14 +274,25 @@ public class Game {
 		return score;
 	}
 	
+	/**
+	 * Used to get whether the player has won or lost the game
+	 * @return			True if the player won
+	 */
 	public boolean getWinStatus() {
 		return gameWon;
 	}
 	
+	/**
+	 * Sets the player's win status
+	 * @param status	The players win status. True if game won
+	 */
 	public void setWinStatus(boolean status) {
 		gameWon = status;
 	}
-
+	
+	/**
+	 * Used to quit the game
+	 */
 	public void quitGame() {
 		System.exit(0);
 	}
